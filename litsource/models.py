10163736fa4325faf53446f991d2161b0dd0adbc
Mikhail Trunov <mikhail.trunov@gmail.com>
2020-11-27 11:45:03 +0400

    Added url field to source model

--- litsource/models.py
+++ litsource/models.py
@@ -2,11 +2,23 @@ from django.db import models
 
 class LiteratureSource(models.Model):
 
+    
+
     name = models.CharField(blank=False, max_length=100, unique=True)
     # тип источника book, article и т.п.
-    source_type = models.CharField(blank=False, max_length=10, default="art")
+    CHOICES = (        
+        ('art', 'Статья'),
+        ('gost', 'ГОСТ'),          
+    )
+    source_type = models.CharField(blank=False,
+                                   choices=CHOICES,  
+                                   max_length=10, 
+                                   default="art")
     
     latexpart = models.TextField(blank=False)
+
+    source_url = models.URLField(blank=True, null=True,
+                                 max_length=500, verbose_name="Url cсылка на источник (при наличии)")
 
     def __str__(self):
         return self.name
